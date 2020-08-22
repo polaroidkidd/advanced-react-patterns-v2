5ff62ed6ce273d31e1110af152a18aa2e641472d
Daniel Einars <contact@dle.dev>
2020-08-22 17:10:07 +0200

    completed Provider Pattern: Higher Order Components

--- src/exercises/12.js
+++ src/exercises/12.js
@@ -3,6 +3,7 @@
 import React from 'react'
 // import hoistNonReactStatics from 'hoist-non-react-statics'
 import {Switch} from '../switch'
+import hoistNonReactStatics from 'hoist-non-react-statics'
 
 const ToggleContext = React.createContext({
   on: false,
@@ -122,26 +123,15 @@ class Toggle extends React.Component {
 }
 
 function withToggle(Component) {
-  return Component
-  // The `withToggle` function is called a "Higher Order Component"
-  // It's another way to share code and allows you to statically
-  // create new components to render.
-  // The basic idea is you create a new component that renders the
-  // component the HOC is given.
-  //
-  // This presents a few issues that we'll have to deal with in our
-  // component.
-  //
-  // 1. 🐨 create and return a function component called "Wrapper" which renders
-  //    a <Toggle.Consumer> with a child function that renders <Component />
-  //    with the props Wrapper is given as well as a toggle prop
-  // 2. 🐨 Handle `ref`s properly by using React.forwardRef:
-  //    https://reactjs.org/docs/forwarding-refs.html
-  // 3. 🐨 Make it easier to debug using the React DevTools by setting a
-  //    useful `displayName` property on the Wrapper.
-  // 4. 🐨 Use the `hoistNonReactStatics` function (uncomment the imported above)
-  //    by calling it with the Wrapper and the Component to forward all the
-  //    static properties from the Component to the Wrapper
+  const Wrapper = (props, ref) => {
+    return (
+      <Toggle.Consumer>
+        {toggleUtils => <Component toggle={toggleUtils} ref={ref} {...props}/>}
+      </Toggle.Consumer>
+    )
+  }
+  Wrapper.displayName = `withToggle(${Component.displayName || Component.name})`
+  return hoistNonReactStatics(React.forwardRef(Wrapper), Component)
 }
 
 /////////////////////////////////////////////////////////
